23663a7882e7635c1d48399c7a3c4154cc00a148
chrisputzu <christian.putzu.job@gmail.com>
2026-04-14 15:54:41 +0200

    fix: fix missing data and remove secrets

--- module-1/api_integration.sql
+++ module-1/api_integration.sql
@@ -3,23 +3,24 @@ CREATE DATABASE course_repo;
 USE SCHEMA public;
 
 -- Create credentials
+-- Replace ${GITHUB_PAT} with your actual GitHub Personal Access Token from .env.local
 CREATE OR REPLACE SECRET course_repo.public.github_pat
   TYPE = password
-  USERNAME = ''
-  PASSWORD = '';
+  USERNAME = 'christian-putzu-ey'
+  PASSWORD = '${GITHUB_PAT}';
 
 -- Create the API integration
-CREATE OR REPLACE API INTEGRATION
+CREATE OR REPLACE API INTEGRATION git_api_integration
   API_PROVIDER = git_https_api
-  API_ALLOWED_PREFIXES = ('') -- URL to your GitHub profile
-  ALLOWED_AUTHENTICATION_SECRETS = ()
+  API_ALLOWED_PREFIXES = ('https://github.com/christian-putzu-ey') -- URL to your GitHub profile
+  ALLOWED_AUTHENTICATION_SECRETS = (github_pat)
   ENABLED = TRUE;
 
 -- Create the git repository object
 CREATE OR REPLACE GIT REPOSITORY course_repo.public.advanced_data_engineering_snowflake
-  API_INTEGRATION =  -- Name of the API integration defined above
-  ORIGIN = '' -- Insert URL of forked repo
-  GIT_CREDENTIALS = ;
+  API_INTEGRATION = git_api_integration -- Name of the API integration defined above
+  ORIGIN = 'https://github.com/christian-putzu-ey/advanced-data-engineering-snowflake.git' -- Insert URL of forked repo
+  GIT_CREDENTIALS = course_repo.public.github_pat;
 
 -- List the git repositories
 SHOW GIT REPOSITORIES;
